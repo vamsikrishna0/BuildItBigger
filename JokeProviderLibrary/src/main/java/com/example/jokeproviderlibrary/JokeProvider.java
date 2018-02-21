9b9221f180a3e4c6367d1bcbd6a40b56a885e18a
vamsikrishna0 <a.vamsikrishna0@gmail.com>
2018-02-20 22:20:55 -0500

    After Step 1.

---
+++ JokeProviderLibrary/src/main/java/com/example/jokeproviderlibrary/JokeProvider.java
@@ -0,0 +1,11 @@
+package com.example.jokeproviderlibrary;
+
+public class JokeProvider {
+    public static void main(String[] args) {
+        System.out.println(tellJoke());
+    }
+    public static String tellJoke(){
+        return "Knock Knock! \nWho's there?\nA broken pencil.\nA broken pencil who?\nNever mind it's pointless.";
+    }
+}
+
